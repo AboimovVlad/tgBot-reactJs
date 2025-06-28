c97ccddba6625297a20053e3c857fcb3db519211
Aboimov Vlad Notebook <aboimovvlad7@gmail.com>
2025-06-28 23:15:44 +0300

    kj

--- mini-app-react/src/App.js
+++ mini-app-react/src/App.js
@@ -2,14 +2,10 @@ import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    tg.ready();
-  }, [])
-
+  
   return (
     <div className="App">
       work
-    <button onClick={onClose}>закыть</button>
     </div>
   );
 }
