6e3d82724488b1fec0b33e143b5986fbf4516bed
Aboimov Vlad Notebook <aboimovvlad7@gmail.com>
2025-06-29 02:51:22 +0300

    kk

--- mini-app-react/src/components/Form/Form.jsx
+++ mini-app-react/src/components/Form/Form.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Form.css'
+import { useTelegram } from '../../hooks/useTelegram';
 
 export default function Form() {
-  return (
-    <div>
-        form
-    </div>
+    const [country, setCountry] = useState('');
+    const [street, setstreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'отправить данные'
+        })
+    }, []);
+
+    useEffect(() => {
+        if (!street || !country){
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street]);
+
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeStreet = (e) => {
+        setstreet(e.target.value)
+    }
+
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
+
+    return (
+        <div className='form'>
+            <h3>Введите ваши данные</h3>
+            <input
+             className='input'
+             type="text"
+             placeholder='страна'
+             value={country}
+             onChange={onChangeCountry}
+            />
+
+            <input
+             className='input'
+             type="text"
+             placeholder='улица'
+             value={street}
+             onChange={onChangeStreet}
+            />
+
+            <select className='select' value={subject} onChange={onChangeSubject}>
+                <option value="physical">Физ. лицо</option>
+                <option value="legel">Юр. лицо</option>
+            </select>
+        </div>
   )
 }
