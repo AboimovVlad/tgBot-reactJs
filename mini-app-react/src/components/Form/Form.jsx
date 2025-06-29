1f2eca7f5c59c489bec4c820a6a870d91805c105
Aboimov Vlad Notebook <aboimovvlad7@gmail.com>
2025-06-29 03:55:53 +0300

    ok

--- mini-app-react/src/components/Form/Form.jsx
+++ mini-app-react/src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import './Form.css'
 import { useTelegram } from '../../hooks/useTelegram';
 
@@ -7,6 +7,22 @@ export default function Form() {
     const [street, setstreet] = useState('');
     const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [tg]) 
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [tg])
 
     useEffect(() => {
         tg.MainButton.setParams({
