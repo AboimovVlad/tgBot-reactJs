36a4c233ad186d99f125906f10dd298c78ab1f35
Aboimov Vlad Notebook <aboimovvlad7@gmail.com>
2025-06-29 12:06:29 +0300

    ok

--- mini-app-react/src/components/ProductList/ProductList.jsx
+++ mini-app-react/src/components/ProductList/ProductList.jsx
@@ -4,14 +4,14 @@ import ProductItem from '../ProductItem/ProductItem'
 import { useTelegram } from '../../hooks/useTelegram';
 
 const products = [
-    {id: '1', title: 'джинсы', price: 5000, descriotion: 'синего цвета, прямые'},
-    {id: '2', title: 'куртка', price: 12000, descriotion: 'зеленого цвета, теплая'},
-    {id: '3', title: 'джинсы 2', price: 5000, descriotion: 'синего цвета, прямые'},
-    {id: '4', title: 'куртка 8', price: 122, descriotion: 'зеленого цвета, теплая'},
-    {id: '5', title: 'джинсы 3', price: 5000, descriotion: 'синего цвета, прямые'},
-    {id: '6', title: 'куртка 4', price: 600, descriotion: 'зеленого цвета, теплая'},
-    {id: '7', title: 'джинсы 4', price: 5500, descriotion: 'синего цвета, прямые'},
-    {id: '8', title: 'куртка 5', price: 12000, descriotion: 'зеленого цвета, теплая'}
+    {id: '1', title: 'джинсы', price: 5000, description: 'синего цвета, прямые'},
+    {id: '2', title: 'куртка', price: 12000, description: 'зеленого цвета, теплая'},
+    {id: '3', title: 'джинсы 2', price: 5000, description: 'синего цвета, прямые'},
+    {id: '4', title: 'куртка 8', price: 122, description: 'зеленого цвета, теплая'},
+    {id: '5', title: 'джинсы 3', price: 5000, description: 'синего цвета, прямые'},
+    {id: '6', title: 'куртка 4', price: 600, description: 'зеленого цвета, теплая'},
+    {id: '7', title: 'джинсы 4', price: 5500, description: 'синего цвета, прямые'},
+    {id: '8', title: 'куртка 5', price: 12000, description: 'зеленого цвета, теплая'}
 ]
 
 const getTotalPrice = (item = []) => {
@@ -47,12 +47,15 @@ export default function ProductList() {
 
   return (
     <div className='list'>
-        {products.map(item => {
-            <ProductItem
-             product={item}
-             onAdd={onAdd}
-             className={'item'} />
-        })}
+        {products.map(item => (
+        <ProductItem
+            key={item.id}
+            product={item}
+            onAdd={onAdd}
+            className={'item'}
+        />
+))}
+
     </div>
   )
 }
